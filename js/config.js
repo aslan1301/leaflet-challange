bcef2390dc0836e13dfc4219349d4f1fd635428a
aslan1301 <mercannaslan@gmail.com>
2021-04-30 19:23:26 -0400

    new

---
+++ js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiYXNsYW5tMTEzIiwiYSI6ImNrbzR4b2o1NzE4emgybm1iNGtyZmdpbnkifQ.Wmdrk_I3rB42gpbZ5_Aawg";
--- static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiYXNsYW5tMTEzIiwiYSI6ImNrbTE4cjV1YzB0b2UydXFzMGprNWZ6NmcifQ._Xc6njqdhDqwYXgUj9h9HA";
